b5b1742298dbc8574fc04ee4f86a61658a3649e9
Wesley Xavier <wesley.xvier@gmail.com>
2026-08-20 10:49:16 -0300

    chore: restructure project into backend and update deps

---
+++ backend/src/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    model_config = ConfigDict(from_attributes=True)
