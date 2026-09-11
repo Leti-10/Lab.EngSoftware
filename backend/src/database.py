417bedb4f3ad9f56ff543872ff9ed48f0796e785
Wesley Xavier <wesley.xvier@gmail.com>
2026-09-11 00:51:11 -0300

    fix(config): corrige imports e configuração do backend

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from settings import Settings
+from src.settings import Settings
 
 settings = Settings()
 async_engine = create_async_engine(settings.DATABASE_URL)
--- backend/src/security.py
+++ backend/src/security.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 
 from http import HTTPStatus
 
-from fastapi import Depends, HTTPException, HTTPException
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 
 from jwt import DecodeError, decode, encode, ExpiredSignatureError
@@ -12,9 +12,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from pwdlib import PasswordHash
 
-from model.models import User
-from database import get_session
-from settings import Settings
+from src.models.user import User
+from src.database import get_session
+from src.settings import Settings
 
 pwd_context = PasswordHash.recommended()
 settings = Settings()
@@ -58,7 +58,7 @@ async def get_current_user(
         if not subject_email:
             raise credentials_exception
 
-    except DecodeError, ExpiredSignatureError:
+    except (DecodeError, ExpiredSignatureError):
         raise credentials_exception
 
     user = await session.scalar(select(User).where(User.email == subject_email))
