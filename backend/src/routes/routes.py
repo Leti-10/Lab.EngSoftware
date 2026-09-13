b5d903c426d3128ee1ed52f38bb26e85728d7f45
Wesley Xavier <wesley.xvier@gmail.com>
2026-09-13 16:39:03 -0300

    refactor!: reset da estrutura do projeto

--- backend/src/routes/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from src.controller.auth_controller import router as auth_router
-from src.controller.user_controller import router as user_router
-
-
-api_router = APIRouter()
-api_router.include_router(auth_router)
-api_router.include_router(user_router)
--- backend/src/schemas/schema.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class TokenSchema(BaseModel):
-    token_type: str
-    access_token: str
