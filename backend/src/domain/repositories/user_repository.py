9871acd18e7635af4065acc8dd70334dd98d2d4c
Wesley Xavier <wesley.xvier@gmail.com>
2026-09-13 22:46:32 -0300

    feat(domain): adiciona user entity e repository

--- backend/src/domain/repositories/user_repository.py
+++ backend/src/domain/repositories/user_repository.py
@@ -5,13 +5,13 @@ from backend.src.domain.entities.user import User
 
 class UserRepository(ABC):
     @abstractmethod
-    def create_user(self, user: User) -> User:
+    def save(self, user: User) -> User:
         pass
 
     @abstractmethod
-    def get_user_by_id(self, user_id: int) -> Optional[User]:
+    def get_by_id(self, user_id: int) -> User | None:
         pass
 
     @abstractmethod
-    def get_all_users(self) -> List[User]:
+    def get_by_email(self, email: str) -> User | None:
         pass
