34cdb8c8aa1f9ec3f6e19dde21b80af57b7546a6
Wesley Xavier <wesley.xvier@gmail.com>
2026-09-14 00:20:04 -0300

    feat(usecase): implementa caso de uso create_user

---
+++ backend/src/application/use_cases/user/create_user.py
@@ -0,0 +1,27 @@
+from src.domain.entities.user import User
+from src.domain.repositories.user_repository import UserRepository
+from src.infrastructure.security.password_hasher import PasswordHasher
+
+
+class CreateUserUseCase:
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
+
+    def execute(
+        self, username: str, email: str, password: str, role: str = "user"
+    ) -> User:
+        existing_user = self.user_repository.get_by_email(email)
+
+        if existing_user:
+            raise ValueError("E-mail já cadastrado")
+
+        user = User(
+            username=username,
+            email=email,
+            password=PasswordHasher.hash_password(password),
+            role=role,
+        )
+
+        saved_user = self.user_repository.save(user)
+
+        return saved_user
