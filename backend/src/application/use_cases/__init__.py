2b2cc17d8975e6af9426eaf50020912bb6e8ba34
Wesley Xavier <wesley.xvier@gmail.com>
2026-09-24 01:33:02 -0300

    refactor: melhora a clareza dos imports

---
+++ backend/src/application/use_cases/__init__.py
@@ -0,0 +1,5 @@
+from .book.create_book import CreateBookUseCase
+from .book_list.create_list import CreateBookListUseCase
+from .user.create_user import CreateUserUseCase
+
+__all__ = ["CreateBookUseCase", "CreateBookListUseCase", "CreateUserUseCase"]
--- backend/src/domain/entities/__init__.py
+++ backend/src/domain/entities/__init__.py
@@ -1,5 +1,5 @@
 from .book import Book
 from .user import User
-from .list import BookList
+from .book_list import BookList
 
 __all__ = ["Book", "User", "BookList"]
