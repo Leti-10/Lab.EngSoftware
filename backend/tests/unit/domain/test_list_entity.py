5fdecd46e32702609001b62a78e51540c547f415
Wesley Xavier <wesley.xvier@gmail.com>
2026-09-23 23:37:12 -0300

    test(entity): adiciona teste de criação e lista

---
+++ backend/tests/unit/domain/test_list_entity.py
@@ -0,0 +1,33 @@
+import pytest
+
+from src.domain.entities import DataList
+from src.domain.exceptions import InvalidListNameError, InvalidListOwnerError
+
+
+def test_create_list_successfully():
+
+    owner = 1
+    name = "my_list"
+    description = "generic_list_of_books"
+    private = True
+    books = [1, 2]
+
+    new_list = DataList(owner, name, description, private, books)
+
+    assert new_list.owner == owner
+    assert new_list.name == name
+    assert new_list.description == description
+    assert new_list.private is private
+    assert new_list.books == books
+
+
+def test_should_not_create_list_with_short_name():
+    with pytest.raises(InvalidListNameError):
+        DataList(owner=1, name="ab", description="desc", private=True, book_ids=[])
+
+
+def test_should_not_create_list_without_owner():
+    with pytest.raises(InvalidListOwnerError):
+        DataList(
+            owner=None, name="generic", description="desc", private=True, book_ids=[]
+        )
